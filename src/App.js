ba08fc5f10c06d3bd229b84df0a1a195ae02b025
AdityaChourey <aditya77316@gmail.com>
2021-11-12 18:21:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+import SetupForm from './SetupForm'
+import Loading from './Loading'
+import Modal from './Modal'
+function App() {
+  const {waiting,loading,questions,index,correct,nextQuestion,checkAnswers} = useGlobalContext()
+  if(waiting){
+    return <SetupForm/>
+  }
+  if(loading){
+    return <Loading/>
+  }
+  console.log(questions[0])
+  const {question,correct_answer,incorrect_answers} = questions[index];
+  const answers = [...incorrect_answers]
+  const tempIndex = Math.floor(Math.random()*4)
+  if(tempIndex===3){
+    answers.push(correct_answer)
+  }
+  else{
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
+  return <main>
+    <Modal/>
+    <section className="quiz">
+      <p className='correct-answers'>
+        correct answer : {correct}/{index}
+      </p>
+      <article className="container">
+        <h2 dangerouslySetInnerHTML={{__html:question}}></h2>
+        <div className="btn-container">
+          {answers.map((answer,index)=>{
+            return <button className="answer-btn" key={index} 
+            dangerouslySetInnerHTML={{__html:answer}}
+            onClick={()=>checkAnswers(correct_answer===answer)}></button>
+          })}
+        </div>
+      </article>
+      <button className="next-question" onClick={nextQuestion}>
+        next question
+      </button>
+    </section>
+  </main>
+}
+
+export default App
